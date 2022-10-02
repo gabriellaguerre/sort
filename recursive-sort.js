8613f1a9717c2f760bda544c0c8b06ac3e385cf1
gabriellaguerre <laguerre.g.g@gmail.com>
2022-10-02 17:27:03 -0400

    recursive-sort

---
+++ recursive-sort.js
@@ -0,0 +1,20 @@
+function recursiveSort(arr) {
+
+    if (arr.length <= 1) return arr;
+
+    let maxIndex = 0;
+    for (let i = 1 ; i < arr.length ; i++) {
+        if (arr[i] > arr[maxIndex]) maxIndex = i;
+    }
+    const maxValue = arr[maxIndex];
+    arr.splice(maxIndex, 1);
+
+    arr = recursiveSort(arr);
+
+    arr.push(maxValue);
+
+    return arr;
+}
+
+let arr = [1, 4, 2, 5, 3, 8, 9,]
+console.log(recursiveSort(arr))
